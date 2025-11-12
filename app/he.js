7869ea96c8ba67e2d6c57512953964c6d6a9cf1b
ajharul <mdajharulislam327@gmail.com>
2025-11-13 01:05:13 +0600

    add a new feature

---
+++ app/he.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const He = () => {
+  return (
+    <div>
+      this is he page
+    </div>
+  )
+}
+
+export default He
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-
+import He from "./he"
 export default function page(){
 
   
   return(
-    <div className="text-3xl">hello</div>
+    <div className="text-3xl">hello
+    <He/>
+    </div>
+    
   )
 }
