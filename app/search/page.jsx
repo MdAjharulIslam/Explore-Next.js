3510f1dfd2b05fb24780d141bea1be9a41ecebc4
ajharul <mdajharulislam327@gmail.com>
2025-11-17 01:14:30 +0600

    formet

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -12,29 +12,35 @@ const page = () => {
     <div>
       <div>
         <h1>Search posts</h1>
-        <input type="text" name="" id="" onChange={(e)=>setQuiry(e.target.value)} />
-        {
-        filterPosts.length > 0 ?
-        filterPosts.map((post) => {
-         return <div key={post.id}>
-            <p>{post.title}</p>
-            <p>{post.category}</p>
-          </div>
-        }): <h1>no posts</h1>
-        }
-        
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => setQuiry(e.target.value)}
+        />
+        {filterPosts.length > 0 ? (
+          filterPosts.map((post) => {
+            return (
+              <div key={post.id}>
+                <p>{post.title}</p>
+                <p>{post.category}</p>
+              </div>
+            );
+          })
+        ) : (
+          <h1>no posts</h1>
+        )}
       </div>
       <div>
         <h1>All posts</h1>
-        {
-            posts.map((post)=>{
-               return <div key={post.id}>
-                    <p>{post.title}</p>
-                    <p>{post.category}</p>
-
-                </div>
-            })
-        }
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <p>{post.category}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
