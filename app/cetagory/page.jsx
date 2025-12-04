57720291a6b4651d55b834940972561ecce518a8
ajharul <mdajharulislam327@gmail.com>
2025-12-04 18:38:55 +0600

    new cetagory added successfully

--- app/cetagory/page.jsx
+++ app/cetagory/page.jsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import { posts } from "../search/post.js";
 import Link from "next/link.js";
 const page = () => {
- 
   const cetagory = ["CSS", "Programming", "Frontend", "Web-Dev"];
-  
+  const [c, setc] = useState("");
+  const filterdPost = posts.filter(
+    (post) => post.category.toLowerCase() === c.toLowerCase()
+  );
 
   return (
     <div>
@@ -18,16 +20,36 @@ const page = () => {
           </div>
         );
       })}
-<br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div>
         {cetagory.map((k, index) => {
-          return <li key={index} >
-          <Link href={`/cetagory/${k}`}>{k}</Link>
-          </li>
+          return (
+            <li key={index}>
+              <Link href={`/cetagory/${k}`}>{k}</Link>
+
+              {/* <h1 onClick={() => setc(k)}>{k}</h1> */}
+            </li>
+          );
         })}
       </div>
-      <br /><br /><br /><br />
-      
+      <br />
+      <br />
+      <br />
+      <br />
+
+      {/* <div>
+        {filterdPost.map((ca, index) => {
+          return (
+            <div key={index}>
+              <h1>{ca.id}</h1>
+              <h1>{ca.title}</h1>
+            </div>
+          );
+        })}
+      </div> */}
     </div>
   );
 };
